aecf833367b765f410b2e90c8ff2627871b231b5
Aroon1311 <aroon4626@gmail.com>
2024-08-07 22:41:03 +0530

    changes done in git to git server

--- javaproject1/src/icici/loans/persanalloans/GitClass.java
+++ javaproject1/src/icici/loans/persanalloans/GitClass.java
@@ -6,6 +6,7 @@ public class GitClass {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
         System.out.println("change in git");
+        System.out.println("chaned in git server");
 	}
 
 }
